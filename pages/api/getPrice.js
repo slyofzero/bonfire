9b13391edfe9682279cbb61619253f0c09ad1716
Vortex Dapp <nai.work1212@gmail.com>
2025-01-15 17:45:30 +0000

    Update getPrice.js

--- pages/api/getPrice.js
+++ pages/api/getPrice.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export default async function handler(req, res) {
   try {
     const resp = await axios.get(
-      "https://api.dexview.com/pair?baseToken=&chainId=1",
+      "https://api.dexview.com/pair?baseToken=0x16D07efb7bc3A3BdEA09F19EC5fB4178e640E089&chainId=1",
       {
         headers: {
           secret: "5ff3a258-2700-11ed-a261-0242ac120002",
